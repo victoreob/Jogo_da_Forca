d36cf388e635ef4cbe3f2bc64643695eaf1609ce
Victor Eduardo Bittencourt <victor.eduardo@cwi.com.br>
2016-11-11 16:50:02 -0200

    feat(JogoForca): inserir estrutura inicial do projeto
    Divido em 1. WebAPI, 2. Domínio, 3. Repositorio

---
+++ JogoForca/JogoForca/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace JogoForca.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ JogoForca/JogoForca/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JogoForca.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
