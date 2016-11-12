7f07b5bf1cbc13313a84bf2fcf81a2ee2270df46
Unknown <gabrield47@gmail.com>
2016-11-12 12:35:57 -0200

    feat(BundleConfig): adicionar scripts da pasta app para o bundle.

--- JogoForca/JogoForca/App_Start/BundleConfig.cs
+++ JogoForca/JogoForca/App_Start/BundleConfig.cs
@@ -11,6 +11,14 @@ namespace JogoForca
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app/usuarios.js",
+               "~/Scripts/app/palavras.js",
+               "~/Scripts/app/telaPrincipal.js",
+               "~/Scripts/app/telaLogin.js",
+               "~/Scripts/app/jogoDaForca.js",
+               "~/Scripts/app/_start.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
