f1debca6f87c035fe5c006478d10d04b1d6d046b
Unknown <gabrield47@gmail.com>
2016-11-12 12:42:06 -0200

    feat(palavras): adicionar classe com get e post.

---
+++ JogoForca/JogoForca/Scripts/app/palavras.js
@@ -0,0 +1,19 @@
+﻿class Palavras {
+
+  pegarRegistrosNivelNormal() {
+    return $.get('/api/palavras', {
+      palavras: palavras
+    });
+  }
+
+  pegarRegistrosNivelBh() {
+    return $.get('/api/palavras/bh', {
+      palavras: palavras
+    });
+  }
+
+  cadastrar(palavra) {
+    return $.post('/api/palavras', palavra);
+  }
+
+}
