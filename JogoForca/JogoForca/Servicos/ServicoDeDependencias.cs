8a15c5c1e146b3a76aaeb50e1e99ad0dcd4f0c6a
Unknown <gabrield47@gmail.com>
2016-11-12 09:19:14 -0200

    feat(ServicoDeDependencias): adicionar dependencias.

---
+++ JogoForca/JogoForca/Servicos/ServicoDeDependencias.cs
@@ -0,0 +1,22 @@
+﻿using JogoForca.Dominio.Repositorio;
+using JogoForca.Repositorio.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JogoForca.Servicos
+{
+    public class ServicoDeDependencias
+    {
+        public static IPalavraRepositorio MontarPalavraRepositorio()
+        {
+            return new PalavraRepositorio();
+        }
+
+        public static IUsuarioRepositorio MontarUsuarioRepositorio()
+        {
+            return new UsuarioRepositorio();
+        }
+    }
+}
