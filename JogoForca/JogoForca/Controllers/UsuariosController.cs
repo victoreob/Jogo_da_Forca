6e32752644aea86e7b6160a38fc4d1cd962a5694
Victor <victor_eob@hotmail.com>
2016-11-13 00:03:16 -0200

    feat(UsuarioController): inserir lógica para PostUsuario() - CriarUsuarioNoBanco
    TO-DO: outros métodos

---
+++ JogoForca/JogoForca/Controllers/UsuariosController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using JogoForca.Dominio.Models;
+using JogoForca.Repositorio;
+using JogoForca.Servicos;
+
+namespace JogoForca.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        private ContextoDeDados db = new ContextoDeDados();
+
+        // POST: api/Usuarios
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult PostUsuario(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            ServicoDeDependencias.MontarUsuarioRepositorio().CriarUsuario(usuario);
+
+            //await db.SaveChangesAsync();
+            return CreatedAtRoute("DefaultApi", new { id = usuario.Id }, usuario);
+        }
+
+
+
+        // GET: api/Usuarios
+        public IQueryable<Usuario> GetUsuario()
+        {
+
+
+            return db.Usuario;
+        }
+
+        // GET: api/Usuarios/5
+        [ResponseType(typeof(Usuario))]
+        public async Task<IHttpActionResult> GetUsuario(int id)
+        {
+            Usuario usuario = await db.Usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        // PUT: api/Usuarios/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutUsuario(int id, Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != usuario.Id)
+            {
+                return BadRequest();
+            }
+
+
+            db.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/Usuarios/5
+        [ResponseType(typeof(Usuario))]
+        public async Task<IHttpActionResult> DeleteUsuario(int id)
+        {
+            Usuario usuario = await db.Usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            db.Usuario.Remove(usuario);
+            await db.SaveChangesAsync();
+
+            return Ok(usuario);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return db.Usuario.Count(e => e.Id == id) > 0;
+        }
+    }
+}
