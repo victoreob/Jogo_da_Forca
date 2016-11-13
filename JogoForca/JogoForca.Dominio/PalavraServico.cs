fb27fd5bd821094b5a4e3506bbaa16050a85d222
Victor <victor_eob@hotmail.com>
2016-11-12 23:37:54 -0200

    feat(PalavraServico): inserir lógica para a classe de PalavraServico
    ListaDePalavrasRandom e ListaDePalavrasRandomNivelBH
    Método randomico:         http://pt.stackoverflow.com/questions/86152/embaralhar-n%c3%bameros-de-uma-lista

--- JogoForca/JogoForca.Dominio/PalavraServico.cs
+++ JogoForca/JogoForca.Dominio/PalavraServico.cs
@@ -1,4 +1,5 @@
-﻿using JogoForca.Dominio.Repositorio;
+﻿using JogoForca.Dominio.Models;
+using JogoForca.Dominio.Repositorio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,8 +19,39 @@ namespace JogoForca.Dominio
         }
 
 
+        // CODIGO DE RANDOM EM
+        // http://pt.stackoverflow.com/questions/86152/embaralhar-n%c3%bameros-de-uma-lista
 
+        public IList<Palavra> ListaDePalavrasRandom()
+        {
+            IList<Palavra> listaRandomica;
+            listaRandomica = palavraRepositorio.ListaDePalavras();
 
+            var random = new Random();
 
+            var query =
+                from i in listaRandomica
+                let r = random.Next()
+                orderby r
+                select i;
+
+            return query.ToArray();
+        }
+
+        public IList<Palavra> ListaDePalavrasRandomNivelBH()
+        {
+            IList<Palavra> listaRandomica;
+            listaRandomica = palavraRepositorio.ListaDePalavrasCom12Caracteres();
+
+            var random = new Random();
+
+            var query =
+                from i in listaRandomica
+                let r = random.Next()
+                orderby r
+                select i;
+
+            return query.ToArray();
+        }
     }
 }
