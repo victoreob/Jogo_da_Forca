510431896cd767cfe116c91c7a90d05de43da2f6
Unknown <gabrield47@gmail.com>
2016-11-14 13:27:22 -0200

    feat(_start): adicionar prototype para storage.

--- JogoForca/JogoForca/Scripts/app/_start.js
+++ JogoForca/JogoForca/Scripts/app/_start.js
@@ -1,3 +1,9 @@
 ﻿$(function () {
+  Storage.prototype.setObj = function (key, obj) {
+    return this.setItem(key, JSON.stringify(obj));
+  };
+  Storage.prototype.getObj = function (key) {
+    return JSON.parse(this.getItem(key));
+  };
   jogoDaForca.iniciar();
 });
